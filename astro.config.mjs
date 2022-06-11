b89520863643e71fafbad841a289029e59bc1d11
yend724 <yend9524@gmail.com>
2022-06-12 00:30:09 +0900

    Update style

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://yend724.github.io/',
-  base: '/learning-coding-math'
+  site: 'https://yend724.github.io',
+  base: '/learning-coding-math/',
+  trailingSlash: 'always',
+  markdown: {
+    drafts: true,
+  }
 });
