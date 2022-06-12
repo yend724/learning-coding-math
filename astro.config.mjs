187e1b836b58af9b6d57765f21f13d41993dac33
yend724 <yend9524@gmail.com>
2022-06-12 12:51:02 +0900

    Update post

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://yend724.github.io',
-  base: '/learning-coding-math/',
+  base: '/tsumic',
   trailingSlash: 'always',
   markdown: {
     drafts: true,
