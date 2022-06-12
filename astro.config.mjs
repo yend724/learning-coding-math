3b4b15d2224904d3834d376215cf4bca79d5fa52
yend724 <yend9524@gmail.com>
2022-06-12 14:54:28 +0900

    Update domains

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://yend724.github.io',
-  base: '/tsumic',
+  site: 'https://tsumic.pages.dev',
+  base: '/',
   trailingSlash: 'always',
   markdown: {
     drafts: true,
